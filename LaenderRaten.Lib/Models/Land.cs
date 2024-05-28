8b9bde21b15074408545aca96111d3e20ecf5f3f
Thomas Stöckl <TStoeckl@schueler.hakzell.at>
2024-05-28 21:00:47 +0200

    Projektdateien hinzufügen.

---
+++ LaenderRaten.Lib/Models/Land.cs
@@ -0,0 +1,23 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaenderRaten.Lib.Models
+{
+    public partial class Land : ObservableObject
+    {
+        public string CountryName { get; set; }
+        public string CapitalCity { get; set; }
+        public string ImageURL { get; set; }
+
+        public Land(string countryName, string capitalCity, string imageUrl)
+        {
+                this.CountryName = countryName;
+                this.CapitalCity = capitalCity;
+                this.ImageURL = imageUrl;
+        }
+    }
+}
