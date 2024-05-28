1e941a53a3e9d4b1001b5cd813cee82ef72a0e7a
Thomas Stöckl <TStoeckl@schueler.hakzell.at>
2024-05-28 21:13:36 +0200

    add Binding to pages

--- LaenderRaten.Gui/MainPage.xaml.cs
+++ LaenderRaten.Gui/MainPage.xaml.cs
@@ -1,25 +1,19 @@
-﻿namespace LaenderRaten.Gui
+﻿using LaenderRaten.Core.ViewModels;
+
+namespace LaenderRaten.Gui
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
 
-        public MainPage()
+
+        public MainPage(MainViewModel vw)
         {
             InitializeComponent();
+            this.BindingContext = vw;
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
+
     }
-
 }
--- LaenderRaten.Gui/Pages/AddCountry.xaml.cs
+++ LaenderRaten.Gui/Pages/AddCountry.xaml.cs
@@ -1,9 +1,12 @@
+using LaenderRaten.Core.ViewModels;
+
 namespace LaenderRaten.Gui.Pages;
 
 public partial class AddCountry : ContentPage
 {
-	public AddCountry()
+	public AddCountry(AddCountryViewModel vw)
 	{
 		InitializeComponent();
+		this.BindingContext = vw;
 	}
 }
