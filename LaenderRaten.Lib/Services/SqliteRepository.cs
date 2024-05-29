a640e2e2f37cd1bfd756ab124939e00b6822e73f
Thomas Stöckl <thomas.stoeckl98@gmail.com>
2024-05-29 08:29:44 +0200

    add Add-Method

--- LaenderRaten.Lib/Services/SqliteRepository.cs
+++ LaenderRaten.Lib/Services/SqliteRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LaenderRaten.Lib.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,4 +9,32 @@ namespace LaenderRaten.Lib.Services;
 
 public class SqliteRepository
 {
+
+    private string _path;
+
+    public SqliteRepository(string path)
+    {
+        _path = path;
+    }
+
+    public bool Add (Land land)
+    {
+
+        try
+        {
+            using (var context = new MyDbContext(_path))
+            {
+                context.Add(land);
+                context.SaveChanges();
+            }
+            return true;
+        }
+        catch (Exception ex)
+        {
+            System.Diagnostics.Debug.WriteLine(ex.Message);
+            return false;
+        }
+       
+    }
+
 }
