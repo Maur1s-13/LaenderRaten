64636af89aed1d8d6ab415ff87cd89db1c51c6f1
Thomas Stöckl <thomas.stoeckl98@gmail.com>
2024-05-29 08:50:59 +0200

    add add method and change umlaute

--- LaenderRaten.Core/ViewModels/AddCountryViewModel.cs
+++ LaenderRaten.Core/ViewModels/AddCountryViewModel.cs
@@ -1,15 +1,20 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using LaenderRaten.Lib.Models;
+using LaenderRaten.Lib.Interfaces;
 
 namespace LaenderRaten.Core.ViewModels
 {
-    public partial class AddCountryViewModel : ObservableObject
+    public partial class AddCountryViewModel(IRepository repository) : ObservableObject
     {
-
+        IRepository _repository = repository;
+        
         //Liste aller Länder anzeigen 
         //Länder die bereits hinzugefügt worden sind mit grünen Text anzeigen
         //Per Imagebutton anzeigen lassen
@@ -28,5 +33,40 @@ namespace LaenderRaten.Core.ViewModels
         [ObservableProperty]
         public string continent;
 
+
+        [RelayCommand]
+        public void Add()
+        {
+            this.CountryName = ReplaceUmlaute(this.CountryName);
+            
+            this.ImageUrl = this.CountryName + ".png";
+
+            Land country = new(this.CountryName, this.CapitalCity, this.ImageUrl.ToLower(), this.Continent);
+
+            var result = _repository.Add(country);
+
+            if (result)
+            {
+                this.CountryName = string.Empty;
+                this.CapitalCity = string.Empty;
+                this.Continent = string.Empty;
+            }
+
+
+        }
+
+
+        public string ReplaceUmlaute(string input)
+        {
+            string output = input.Replace("ä", "ae")
+                              .Replace("ö", "oe")
+                              .Replace("ü", "ue")
+                              .Replace("Ä", "Ae")
+                              .Replace("Ö", "Oe")
+                              .Replace("Ü", "Ue")
+                              .Replace("ß", "ss");
+
+            return output;
+        }
     }
 }
