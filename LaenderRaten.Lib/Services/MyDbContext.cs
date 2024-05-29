b67dd5a7b4219724500132eb1b8d836995b6ff6b
Thomas Stöckl <thomas.stoeckl98@gmail.com>
2024-05-29 08:25:28 +0200

    add mydbcontext

--- LaenderRaten.Lib/Services/MyDbContext.cs
+++ LaenderRaten.Lib/Services/MyDbContext.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using LaenderRaten.Lib.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LaenderRaten.Lib.Services;
 
-public class MyDbContext
+public class MyDbContext : DbContext
 {
+    DbSet<Land> Countrys { get; set; }
+
+
+    private string _path = string.Empty;
+
+    public MyDbContext(string path)
+    {
+        _path = path;
+
+        SQLitePCL.Batteries_V2.Init();
+        this.Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+
+        Debug.WriteLine(_path);
+
+        optionsBuilder.UseSqlite($"Filename={_path}");
+        
+    }
+
 
 }
