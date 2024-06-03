e244ba2cf40974ad8e33808c17559e251dca7d43
Thomas Stöckl <TStoeckl@schueler.hakzell.at>
2024-06-03 20:42:46 +0200

    add alertservice

---
+++ LaenderRaten.Lib/Interfaces/IAlertService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaenderRaten.Lib.Interfaces;
+
+public interface IAlertService
+{
+    void ShowAlert(string title, string message);
+    Task ShowALertAsync(string title, string message);
+}
